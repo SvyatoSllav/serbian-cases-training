1d353e24945e61656904c9b1eee112f33f86b2af
Aleksey Spirin <spirin.mezhmun@yandex.ru>
2025-01-14 09:04:25 +0300

    refactor: format code and styles - Adjusted indentation in TypeScript and SCSS files. - Verified and corrected formatting in SCSS styles. - Removed unnecessary spaces and ensured consistent code styling.

--- frontend/config/storybook/webpack.config.ts
+++ frontend/config/storybook/webpack.config.ts
@@ -1,5 +1,5 @@
-import webpack from 'webpack';
-import path from 'path';
+import * as webpack from 'webpack';
+import * as path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
